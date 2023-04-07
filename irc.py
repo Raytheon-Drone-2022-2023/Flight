c5567d329edded6571b801405a52d787674617cf
Sam Timmins <60010117+sltimmins@users.noreply.github.com>
2023-04-07 14:38:25 -0500

    oops`

--- irc.py
+++ irc.py
@@ -1,41 +1,20 @@
-import socket
-import sys
-import ssl
+import pydle
+import time
 
-ctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc = ctx.wrap_socket(sock) 
-# server = "irc.root-me.org"
-# channel = "#root-me_challenge"
-server = "irc.freenode.net"
-channel = "#RTXDrone"
-nick = "SMUSam"
-botname = "candy"
+# Simple echo bot.
+class MyOwnBot(pydle.Client):
+    async def on_connect(self):
+         await self.join('#RTXDrone')
 
-def joinchan(chan):
-    irc.send(bytes('JOIN' + chan + '\n', 'UTF-8'))
-    ircmsg = ""
-    while ircmsg.find("Users #RTXDrone") == -1:
-        ircmsg = irc.recv(2048).decode('UTF-8')
-        ircmsg = ircmsg.strip('\n\r')
-        print(ircmsg)
-        
-def sendmsg(msg, target=channel): # sends messages to the target.
-    irc.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
-        
-def joinserv(server):
-    irc.connect((server, 7070))
-    irc.send(bytes('USER ' + nick + ' ' + nick + ' ' + nick + ' ' + nick + '\n', 'UTF-8'))
-    irc.send(bytes("NICK "+ nick +"\n", "UTF-8"))
-    ircmsg = ""
-    while ircmsg.find('Thank you for using freenode!') == -1:
-        ircmsg = irc.recv(2048).decode("UTF-8")
-        ircmsg = ircmsg.strip('\n\r')
-        print(ircmsg)
-        
-def main():
-    joinserv(server)
-    joinchan(channel)
-    sendmsg('hello?', channel)
-    
-main()
+    async def on_message(self, target, source, message):
+         # don't respond to our own messages, as this leads to a positive feedback loop
+         if source != self.nickname:
+            await self.message(target, 'test')
+            
+    async def message(self, message):
+        await self.message('#RTXDrone', message)
+
+client = MyOwnBot('MyBot', realname='My Bot')
+client.run('chat.freenode.net', tls=True, tls_verify=False)
+time.sleep(5)
+client.message('custom')
