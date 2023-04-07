a7d04605767aac1ad73b290a1c28f0343a3433cf
Sam Timmins <60010117+sltimmins@users.noreply.github.com>
2023-04-07 14:36:50 -0500

    kinda?

--- irc.py
+++ irc.py
@@ -1,16 +1,21 @@
 import socket
 import sys
+import ssl
 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = "irc.root-me.org"
-channel = "#root-me_challenge"
+ctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc = ctx.wrap_socket(sock) 
+# server = "irc.root-me.org"
+# channel = "#root-me_challenge"
+server = "irc.freenode.net"
+channel = "#RTXDrone"
 nick = "SMUSam"
 botname = "candy"
 
 def joinchan(chan):
     irc.send(bytes('JOIN' + chan + '\n', 'UTF-8'))
     ircmsg = ""
-    while ircmsg.find("End of /NAMES list.") == -1:
+    while ircmsg.find("Users #RTXDrone") == -1:
         ircmsg = irc.recv(2048).decode('UTF-8')
         ircmsg = ircmsg.strip('\n\r')
         print(ircmsg)
@@ -19,18 +24,18 @@ def sendmsg(msg, target=channel): # sends messages to the target.
     irc.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
         
 def joinserv(server):
-    irc.connect((server, 6667))
+    irc.connect((server, 7070))
     irc.send(bytes('USER ' + nick + ' ' + nick + ' ' + nick + ' ' + nick + '\n', 'UTF-8'))
     irc.send(bytes("NICK "+ nick +"\n", "UTF-8"))
-    # ircmsg = ""
-    # while ircmsg.find('MODE SMU') ==-1:
-    #     ircmsg = irc.recv(2048).decode("UTF-8")
-    #     ircmsg = ircmsg.strip('\n\r')
-    #     print(ircmsg)
+    ircmsg = ""
+    while ircmsg.find('Thank you for using freenode!') == -1:
+        ircmsg = irc.recv(2048).decode("UTF-8")
+        ircmsg = ircmsg.strip('\n\r')
+        print(ircmsg)
         
 def main():
     joinserv(server)
     joinchan(channel)
-    sendmsg('hello?', botname)
+    sendmsg('hello?', channel)
     
 main()
