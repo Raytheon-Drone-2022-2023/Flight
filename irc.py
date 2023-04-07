b43599fc34b9f052edeca55d76260e71d9bd3941
Will Strickland <wstrickland@smu.edu>
2023-04-07 14:52:55 -0500

    irc chat mostly working

--- irc.py
+++ irc.py
@@ -5,16 +5,12 @@ import time
 class MyOwnBot(pydle.Client):
     async def on_connect(self):
          await self.join('#RTXDrone')
+         await self.message('#RTXDrone',"RTXDC_2023 [SchoolName]_UAV_Fire_[UGV_ArucoMarkerID]_[timestamp]_[GPS location]")
 
-    async def on_message(self, target, source, message):
-         # don't respond to our own messages, as this leads to a positive feedback loop
-         if source != self.nickname:
-            await self.message(target, 'test')
-            
-    async def message(self, message):
-        await self.message('#RTXDrone', message)
+#     async def on_message(self, target, source, message):
+#          # don't respond to our own messages, as this leads to a positive feedback loop
+#          if source != self.nickname:
+#             await self.message(target, 'test')
 
-client = MyOwnBot('MyBot', realname='My Bot')
+client = MyOwnBot('SMU_PI_BOT', realname='My Bot')
 client.run('chat.freenode.net', tls=True, tls_verify=False)
-time.sleep(5)
-client.message('custom')
